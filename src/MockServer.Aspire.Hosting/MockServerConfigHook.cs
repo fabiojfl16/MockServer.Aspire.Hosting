d2e294e8ea640ffeec72acc3142c01897bcc2cea
Santos Joao (7) <joao.santos.7@volvo.com>
2024-09-19 19:16:30 -0300

    Added retry to check if mockserver container is running.

--- src/MockServer.Aspire.Hosting/MockServerConfigHook.cs
+++ src/MockServer.Aspire.Hosting/MockServerConfigHook.cs
@@ -1,5 +1,7 @@
 ﻿using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Lifecycle;
+using Polly.Retry;
+using Polly;
 using System.Text;
 
 namespace MockServer.Aspire.Hosting;
@@ -13,19 +15,33 @@ internal class MockServerConfigHook : IDistributedApplicationLifecycleHook
         if (!resources.Any())
             return;
 
-        await Task.Delay(500, cancellationToken);
-        using var client = new HttpClient();
+        using var httpClient = new HttpClient();
 
         foreach (var resource in resources)
         {
+            var endpoint = resource.GetEndpoint("http");
+            var url = $"{endpoint.Url}/mockserver/expectation";
+
+            await CheckStatusWithRetryAsync($"{endpoint.Url}/status", new StringContent(string.Empty), httpClient, 3);
+
             foreach (var expectation in resource.Expectations)
             {
-                var endpoint = resource.GetEndpoint("http");
-                var url = $"{endpoint.Url}/mockserver/expectation";
-
                 var content = new StringContent(expectation, Encoding.UTF8, "application/json");
-                await client.PutAsync(url, content, cancellationToken);
+                await httpClient.PutAsync(url, content, cancellationToken);
             }
         }
+    }
+
+    public static async Task<HttpResponseMessage> CheckStatusWithRetryAsync(string url, HttpContent content, HttpClient httpClient, int maxRetries = 3)
+    {
+        AsyncRetryPolicy<HttpResponseMessage> retryPolicy = Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+            .Or<HttpRequestException>()
+            .WaitAndRetryAsync(maxRetries, retryAttempt => TimeSpan.FromSeconds(Math.Pow(1, retryAttempt)),
+                (response, timeSpan, retryCount, context) =>
+                {
+                    Console.WriteLine($"Request to MockServer failed. Waiting {timeSpan} before retry {retryCount}");
+                });
+
+        return await retryPolicy.ExecuteAsync(() => httpClient.PutAsync(url, content));
     }
 }
